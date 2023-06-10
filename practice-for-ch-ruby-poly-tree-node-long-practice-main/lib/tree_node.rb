dec2e246ce9bc08947ffc9d48d93590ee4540a89
dominicchanDmc <dominic@DominicInspiron>
2023-06-09 19:20:23 -0700

    final

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,63 @@
+require 'byebug'
 class PolyTreeNode
-  
+    attr_reader :children,:parent, :value
+
+   def initialize(value)
+    @children = []
+    @parent = nil
+    @value = value
+   end
+   
+   def removeParent()
+    @parent.children.reject!{|node| node == self}
+    @parent = nil
+   end
+
+   def parent=(i_parent)
+        unless i_parent.nil? 
+            if @parent.nil?
+                i_parent.children.push(self) 
+            else
+                removeParent()
+                i_parent.children.push(self)  unless i_parent.children.include?(self)           
+            end
+            @parent = i_parent          
+        else
+            removeParent()
+        end
+   end
+
+    def add_child(child_node)
+        # debugger
+        # @children.push(child_node) if !@children.include?(child_node)           
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        # @children.reject!{|node| node == child_node}
+        # @children.each do |ele|
+        #     ele.parent if ele == child_node
+        raise_error "Not a child" if @children.none?{|c| c == child_node}
+        child_node.parent = nil
+    end
+
+    def dfs(target_value)
+        return self if target_value == self.value
+
+        (0...@children.length).each do |i|
+            node = @children[i].dfs(target_value)
+           return node if !node.nil?
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        # return self if target_value == self.value
+        arry = [self]
+        arry.each do |child|
+           return child if child.value == target_value
+           arry.concat(child.children)
+        end
+        nil
+    end
 end
